d16ae6e574a7a74d234904c7e1895c1a7f602c45
Ivan Shafran <vanobox07@mail.ru>
2015-11-10 16:41:39 +0300

    delete oldfiles

--- src/ru/mail/track/messenger/authorization/UserFieldsCheckingStatus.java
+++
@@ -1,9 +0,0 @@
-package ru.mail.track.messenger.authorization;
-
-/**
- * Created by Ivan Shafran on 29.09.2015.
- * Mail: vanobox07@mail.ru
- */
-public enum UserFieldsCheckingStatus {
-    OK, TOO_SHORT, TOO_LONG, UNALLOWED_SYMBOLS
-}
